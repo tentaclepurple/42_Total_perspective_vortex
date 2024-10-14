e0c47841b02228355efef7c729084681f04b9bf0
Wolfframio <116112114+Wolfframio@users.noreply.github.com>
2024-10-15 00:23:33 +0200

    added ICA

--- utils/Preprocessing.py
+++ utils/Preprocessing.py
@@ -4,14 +4,14 @@
 import mne
 from mne.io import concatenate_raws, read_raw_edf
 from mne.datasets import eegbci
+from mne.preprocessing import ICA
 
-import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def rename_chan(raw) -> None:
-    """
+""" def rename_chan(raw) -> None:
+    '''
     Rename channels in an MNE raw object to match the standard 1005 montage.
 
     Parameters:
@@ -19,7 +19,7 @@ def rename_chan(raw) -> None:
 
     Returns:
         None: The function modifies the `raw` object in place and does not return anything.
-    """
+    '''
     mapping = {
         'Fc5.': 'FC5', 'Fc3.': 'FC3', 'Fc1.': 'FC1', 'Fcz.': 'FCz', 'Fc2.': 'FC2', 'Fc4.': 'FC4', 'Fc6.': 'FC6',
         'C5..': 'C5', 'C3..': 'C3', 'C1..': 'C1', 'Cz..': 'Cz', 'C2..': 'C2', 'C4..': 'C4', 'C6..': 'C6',
@@ -30,7 +30,7 @@ def rename_chan(raw) -> None:
         'P7..': 'P7', 'P5..': 'P5', 'P3..': 'P3', 'P1..': 'P1', 'Pz..': 'Pz', 'P2..': 'P2', 'P4..': 'P4', 'P6..': 'P6', 'P8..': 'P8',
         'Po7.': 'PO7', 'Po3.': 'PO3', 'Poz.': 'POz', 'Po4.': 'PO4', 'Po8.': 'PO8', 'O1..': 'O1', 'Oz..': 'Oz', 'O2..': 'O2', 'Iz..': 'Iz'
     }
-    raw.rename_channels(mapping)
+    raw.rename_channels(mapping) """
 
 
 def load_data(subjects, runs):
@@ -43,7 +43,7 @@ def load_data(subjects, runs):
         raws = [read_raw_edf(f, preload=True) for f in raw_fnames]
         raw = concatenate_raws(raws)
 
-        rename_chan(raw)
+        #rename_chan(raw)
 
         all_raws.append(raw)
 
@@ -52,3 +52,21 @@ def load_data(subjects, runs):
 
     return raw
 
+
+def ica_filter(raw, picks):
+    raw.filter(l_freq=1.0, h_freq=None)
+    ica = mne.preprocessing.ICA(n_components=20, random_state=42)
+    ica.fit(raw, picks=picks)
+    eog_indices, eog_scores = ica.find_bads_eog(raw, ch_name='Fpz.')
+    ica.excluda.extend(eog_indices)
+    ica.apply(raw, exclude=ica.exclude)
+
+
+def filter_alpha_beta(raw):
+    raw.filter(l_freq=8.0, h_freq=30.0)
+
+
+def preprocess(raw, picks):
+    raw_clean = raw.copy()
+    ica_filter(raw_clean, picks)
+    filter_alpha_beta(raw_clean)
